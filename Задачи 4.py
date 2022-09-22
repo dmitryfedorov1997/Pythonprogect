7d882cc9e8b823533392b285e1d280cea8cec903
unknown <zenit07021997@gmail.com>
2022-09-22 14:37:50 +0300

    my second commit

--- Задачи 4.py
+++ Задачи 4.py
@@ -11,7 +11,7 @@ while game != "off":
         for i in  range(0, 3):
             if number == 5:
                 print("Вы выиграли билет на концерт!")
-            else number != 5:
+            else number !=
                 print ("Вы не выиграли билет на концерт!")
     number = input("Введите game (off - завершить):")
 
