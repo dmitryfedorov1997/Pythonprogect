c099f2136cd8fe60bd79736a7d598e3f8a46b02d
dmitryfedorov1997 <zenit07021997@gmail.com>
2022-09-26 13:17:14 +0300

    sorting files

---
+++ Циклы/4.py
@@ -0,0 +1,9 @@
+game = input("Введите game (off - завершить):")
+while game != "off":
+    if game != "game":
+        for i in  range(0, 3):
+            if number == 5:
+                print("Вы выиграли билет на концерт!")
+            else number != 5:
+                print ("Вы не выиграли билет на концерт!")
+    number = input("Введите game (off - завершить):")
