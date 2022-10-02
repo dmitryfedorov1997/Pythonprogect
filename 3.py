1f52801162fd6ef59e8bb6d7b210b264c69dbf9a
dmitryfedorov1997 <114148447+dmitryfedorov1997@users.noreply.github.com>
2022-10-02 22:57:02 +0300

    Множества

---
+++ 3.py
@@ -0,0 +1,11 @@
+c=set()
+newspaper=range(1, 76)
+for newspaper in range(1, 76):
+    c.add(newspaper)
+magazine = range(77, 104)
+for magazine in range(77, 104):
+    c.add(magazine)
+both = range(21, 34)
+for both in range(21, 34):
+    c.add(both)
+print(len(c))
